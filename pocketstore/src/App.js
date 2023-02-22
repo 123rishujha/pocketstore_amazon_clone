3dfc559f23bfc383d4e076b9d0047ca62dfe504f
Parag Sonje <107980794+Parag2510@users.noreply.github.com>
2023-02-22 22:49:10 +0530

    day2

--- pocketstore/src/App.js
+++ pocketstore/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AllRoutes from './routes/AllRoutes';
+import Header from "./components/Header/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <AllRoutes />
     </div>
   );
