5e8888afb587e68c887110120659d211e5790105
Md Jawed <mdjawedh22@gmail.com>
2023-02-23 01:17:35 +0530

    day-2 styling done

--- pocketstore/src/routes/AllRoutes.jsx
+++ pocketstore/src/routes/AllRoutes.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 
 
 const AllRoutes = () => {
@@ -9,7 +11,8 @@ const AllRoutes = () => {
         <Route path='/products' element={<h1>Products</h1>} />
         <Route path='/products/:id' element={<h1>Product-Details</h1>} />
         <Route path='/cart' element={<h1>Cart</h1>} />
-        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>}/>
     </Routes>
   )
 }
