bcb491aacb698229347076fe0d6fb4c232a9ae19
aaka8566 <aakash8566@gmail.com>
2023-02-24 09:50:33 +0530

    p

--- pocketstore/src/routes/AllRoutes.jsx
+++ pocketstore/src/routes/AllRoutes.jsx
@@ -4,7 +4,8 @@ import ProductPage from '../pages/ProductPage/ProductPage';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import MainPage from '../components/HomePage/MainPage';
-
+import Splitscree from "../admin/adminhome";
+import SplitScreen from '../admin/adminlogin';
 
 
 const AllRoutes = () => {
@@ -16,6 +17,8 @@ const AllRoutes = () => {
         <Route path='/cart' element={<h1>Cart</h1>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/adminlogin' element={<SplitScreen/>}/>
+            <Route path='/adminhome' element={<Splitscree/>}/>
     </Routes>
   )
 }
