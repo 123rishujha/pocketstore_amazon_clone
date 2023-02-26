c6da30f09e2523e16df41962eceb8feab935b6d5
Rishu jha <jharishu796@gmail>
2023-02-27 02:19:05 +0530

    details page

--- pocketstore/src/redux/products/product.types.js
+++ pocketstore/src/redux/products/product.types.js
@@ -5,3 +5,6 @@ export const GET_PRODUCTS_SUCCESS = "get/products/success";
 
 // POST REQUEST TYPES
 export const POST_PRODUCTS_SUCCESS = "post/products/success";
+
+//PATCH REQUEST
+export const UPDATE_PRODUCTS = "patch/products/success";
--- pocketstore/src/routes/AllRoutes.jsx
+++ pocketstore/src/routes/AllRoutes.jsx
@@ -8,13 +8,14 @@ import Splitscree from "../admin/adminhome";
 import SplitScreen from '../admin/adminlogin';
 import Adminaddpro from '../admin/adminaddpro';
 import Adminallproducts from '../admin/adminallproducts';
+import ProductDetails from '../pages/ProductDetails/ProductDetails';
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<MainPage/>} />
         <Route path='/products' element={<ProductPage/>} />
-        <Route path='/products/:id' element={<h1>Product-Details</h1>} />
+        <Route path='/products/:id' element={<ProductDetails />} />
         <Route path='/cart' element={<h1>Cart</h1>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<SignUp/>}/>
