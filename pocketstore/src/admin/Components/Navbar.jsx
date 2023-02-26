d207678dfddf03d722779a8c3535da463b6b627e
aaka8566 <aakash8566@gmail.com>
2023-02-26 20:17:30 +0530

    p

--- pocketstore/src/admin/Components/Navbar.jsx
+++ pocketstore/src/admin/Components/Navbar.jsx
@@ -29,7 +29,7 @@ const NavLink = ({ children }) => (
       textDecoration: 'none',
       bg: useColorModeValue('gray.200', 'gray.700'),
     }}
-    href={'#'}>
+    href={children}>
     {children}
   </Link>
 );
